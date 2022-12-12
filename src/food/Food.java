75c6d3c13c3be4aaf07a99644c650924f64932a4
FireChicks <dldi991021@gmail.com>
2022-12-12 17:52:11 +0900

    최근식단 확인까지 구현

---
+++ src/food/Food.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package food;
+
+/**
+ *
+ * @author LeeJanyun
+ */
+public class Food {
+    private String foodName;
+    private String foodBigCategory;
+    private String foodSmallCategory;
+    private int    servingSize;
+    private String sizeUnit;
+    private double kacal;
+    private double moisture;
+    private double protein;
+    private double fat;
+    private double carbohydrate;
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodBigCategory() {
+        return foodBigCategory;
+    }
+
+    public void setFoodBigCategory(String foodBigCategory) {
+        this.foodBigCategory = foodBigCategory;
+    }
+
+    public String getFoodSmallCategory() {
+        return foodSmallCategory;
+    }
+
+    public void setFoodSmallCategory(String foodSmallCategory) {
+        this.foodSmallCategory = foodSmallCategory;
+    }
+
+    public int getServingSize() {
+        return servingSize;
+    }
+
+    public void setServingSize(int servingSize) {
+        this.servingSize = servingSize;
+    }
+
+    public String getSizeUnit() {
+        return sizeUnit;
+    }
+
+    public void setSizeUnit(String sizeUnit) {
+        this.sizeUnit = sizeUnit;
+    }
+
+    public double getMoisture() {
+        return moisture;
+    }
+
+    public void setMoisture(double moisture) {
+        this.moisture = moisture;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getCarbohydrate() {
+        return carbohydrate;
+    }
+
+    public void setCarbohydrate(double carbohydrate) {
+        this.carbohydrate = carbohydrate;
+    }
+
+    public double getKacal() {
+        return kacal;
+    }
+
+    public void setKacal(double kacal) {
+        this.kacal = kacal;
+    }
+    
+    
+    
+    
+}
---
+++ src/food/FoodDAO.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package food;
+
+import diet.Diet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LeeJanyun
+ */
+public class FoodDAO {
+    private Connection conn;
+    private PreparedStatement pstmt;
+    private ResultSet rs;
+
+	public FoodDAO() {
+		try {
+			String dbURL = "jdbc:mysql://125.180.23.159:3306/FP";
+			String dbID = "dldi1021";
+			String dbPassword = "1234";
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+     
+         public ArrayList<Food> searchFoodList(String search) {
+		String SQL = "SELECT * FROM food WHERE foodName = like ?";
+                ArrayList<Food> foodList = new ArrayList<Diet>();
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, "%" + search + "%");
+			rs = pstmt.executeQuery();
+                        while (rs.next()) {
+                            Food food = new Food();
+                            food.setFoodName(rs.getString(1));
+                            food.setFoodBigCategory(rs.getString(2));
+                            food.setFoodSmallCategory(rs.getString(3);
+                            food.setServingSize(rs.getInt(4));
+                            food.setSizeUnit(rs.getString(5));
+                            food.setKacal(rs.getDouble(6));
+                            food.setMoisture(rs.getDouble(7));
+                            food.setProtein(rs.getDouble(8));
+                            food.setFat(rs.getDouble(9));
+                            food.setCarbohydrate(rs.getDouble(10));
+                           foodList.add(food);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return dietList;
+	}
+}
